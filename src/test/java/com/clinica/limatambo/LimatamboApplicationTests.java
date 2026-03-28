408c3d4c81d0c19f12966f0a666f305ba6b8229a
Fabricio-LA <alfaromarcelope@gmail.com>
2026-03-28 02:53:41 -0500

    Estructura inicial: Spring Boot, Thymeleaf, Bootstrap y Estilos

---
+++ src/test/java/com/clinica/limatambo/LimatamboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.limatambo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimatamboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
